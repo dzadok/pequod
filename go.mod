01047753db76c4d7eee4608d6215f2f63bab4407
Dave Zadok <david@dzadok.dev>
2024-04-30 22:40:44 -0400

    wip: Create tui
    When no args display containers and provide tui for updating containers
    When args are passed in run in existing cli mode

--- go.mod
+++ go.mod
@@ -2,8 +2,15 @@ module github.com/dzadok/docker-env
 
 go 1.22.1
 
+require github.com/charmbracelet/bubbletea v0.25.0
+
+require github.com/charmbracelet/bubbles v0.18.0 // indirect
+
 require (
 	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.10.0
+	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/distribution/reference v0.6.0 // indirect
 	github.com/docker/docker v26.0.2+incompatible // indirect
 	github.com/docker/go-connections v0.5.0 // indirect
@@ -12,13 +19,25 @@ require (
 	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.50.0 // indirect
 	go.opentelemetry.io/otel v1.25.0 // indirect
 	go.opentelemetry.io/otel/metric v1.25.0 // indirect
 	go.opentelemetry.io/otel/trace v1.25.0 // indirect
-	golang.org/x/sys v0.1.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/term v0.6.0 // indirect
+	golang.org/x/text v0.3.8 // indirect
 )
--- main.go
+++ main.go
@@ -11,30 +11,118 @@ import (
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
+
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
+type model struct {
+	table table.Model
+}
+
+func (m model) Init() tea.Cmd { return nil }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc":
+			if m.table.Focused() {
+				m.table.Blur()
+			} else {
+				m.table.Focus()
+			}
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			// TODO: figure out capturing input for env var name and value
+			// then call updateEnv with m.table.SelectedRow()[0]
+			return m, tea.Batch(
+				tea.Printf("Let's go to %s!", m.table.SelectedRow()[1]),
+			)
+		}
+	}
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return baseStyle.Render(m.table.View()) + "\n"
+}
+
 func main() {
-	containerName := os.Args[1]
-	envVar := os.Args[2]
 	ctx := context.Background()
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
 		panic(err)
 	}
 	defer cli.Close()
-	containers, err := cli.ContainerList(ctx,
-		container.ListOptions{Filters: filters.NewArgs(filters.Arg("name", "/"+containerName))},
-	)
-	if err != nil {
-		panic(err)
+
+	if len(os.Args) > 1 {
+		containerName := os.Args[1]
+		envVar := os.Args[2]
+		containers, err := cli.ContainerList(ctx,
+			container.ListOptions{Filters: filters.NewArgs(filters.Arg("name", "/"+containerName))},
+		)
+		if err != nil {
+			panic(err)
+		}
+		if len(containers) == 0 {
+			panic("No containers found")
+		}
+		updateEnv(ctx, cli, containers, envVar)
+		fmt.Println("OK")
+
+	} else {
+
+		containers, err := cli.ContainerList(ctx, container.ListOptions{})
+		if err != nil {
+			panic(err)
+		}
+
+		columns := []table.Column{
+			{Title: "ID", Width: 12},
+			{Title: "Name", Width: 30},
+		}
+
+		rows := []table.Row{}
+
+		for _, v := range containers {
+			name, _ := strings.CutPrefix(strings.Join(v.Names, ", "), "/")
+			rows = append(
+				rows,
+				table.Row{
+					v.ID,
+					name,
+				},
+			)
+		}
+
+		t := table.New(
+			table.WithColumns(columns),
+			table.WithRows(rows),
+			table.WithFocused(true),
+			table.WithWidth(80),
+		)
+
+		s := table.DefaultStyles()
+		t.SetStyles(s)
+
+		m := model{t}
+		if _, err := tea.NewProgram(m).Run(); err != nil {
+			panic(err)
+		}
 	}
-	if len(containers) == 0 {
-		panic("No containers found")
-	}
-	updateEnv(ctx, cli, containers, envVar)
-	fmt.Println("OK")
 }
 
+// TODO: Refactor this to accept ids instead of containers
+// that will work much better with the table in the tui
 func updateEnv(ctx context.Context, cli *client.Client, containers []types.Container, envVar string) {
 	varName := strings.Split(envVar, "=")[0]
 	for _, v := range containers {
@@ -45,8 +133,8 @@ func updateEnv(ctx context.Context, cli *client.Client, containers []types.Conta
 		}
 		name := oldContainer.Name
 		found := false
-		for i, v := range oldContainer.Config.Env {
-			if strings.Split(v, "=")[0] == varName {
+		for i, e := range oldContainer.Config.Env {
+			if strings.Split(e, "=")[0] == varName {
 				oldContainer.Config.Env[i] = envVar
 				found = true
 				break
@@ -62,7 +150,7 @@ func updateEnv(ctx context.Context, cli *client.Client, containers []types.Conta
 		newContainer, err := cli.ContainerCreate(ctx,
 			oldContainer.Config,
 			oldContainer.HostConfig,
-			&network.NetworkingConfig{EndpointsConfig: v.NetworkSettings.Networks},
+			&network.NetworkingConfig{EndpointsConfig: oldContainer.NetworkSettings.Networks},
 			nil,
 			"tempname")
 		if err != nil {
